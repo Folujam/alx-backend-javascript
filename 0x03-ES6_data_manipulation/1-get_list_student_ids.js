aeab7b438466289e913386e1646aadbcbc8b5b7e
folujam <olusway@gmail.com>
2024-07-01 23:37:20 +0100

    removed comments and added "function" keyword (omitted)

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,4 @@
-/**
- * function getListStudentIds that returns an array of ids from a list of object
- */
-export default getListStudentIds(students) {
+export default function getListStudentIds(students) {
   if (Array.isArray(students)) {
     return students.map((student) => student.id);
   }

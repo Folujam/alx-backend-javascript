0d273483d2b44e0bdde8adfb29bc9b60144963ac
folujam <olusway@gmail.com>
2024-07-02 01:20:16 +0100

    ready all, test#1

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffa = new DataView(new ArrayBuffer(length), 0, length);
+  buffa.setInt8(position, value);
+  return buffa;
+}

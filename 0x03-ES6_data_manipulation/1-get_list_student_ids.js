501e949e6d88608fdd6181b6d78ea94861e834f3
folujam <olusway@gmail.com>
2024-07-02 21:28:06 +0100

    commented

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,10 @@
+/**
+ * returns a list of students based on id
+ * @param {id, firstName, location} students 
+ * @returns 
+ */
 export default function getListStudentIds(students) {
-  if (students instanceof Array) {
+  if (Array.isArray(students)) {
     return students.map((student) => student.id);
   }
   return [];

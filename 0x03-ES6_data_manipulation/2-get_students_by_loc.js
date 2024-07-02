a54b9262615f172cf62bca02244f6dca3fb86003
folujam <olusway@gmail.com>
2024-07-03 00:18:27 +0100

    test#1

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,9 @@
+/**
+ * returns an array of objects who are located in a specific city.
+ * @param {{}[]} students 
+ * @param {string} city 
+ * @returns 
+ */
 export default function getStudentsByLocation(students, city) {
   if (Array.isArray(students)) {
     return students.filter((student) => student.location === city);

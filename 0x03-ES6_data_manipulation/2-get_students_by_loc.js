1ef69cd313c5fb453d40f1ffef8c9d06ce90dbad
folujam <olusway@gmail.com>
2024-07-03 00:25:48 +0100

    ukno

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,10 @@
 /**
  * returns an array of objects who are located in a specific city.
- * @param {{}[]} students
+ * @param {{
+ * id: Number,
+ * firstName: string,
+ * location: string
+ * }[]} students
  * @param {string} city 
  * @returns 
  */

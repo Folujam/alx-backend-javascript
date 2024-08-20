94db525382dbd61ada3b9a9f259cbd0db1838cfe
folujam <olusway@gmail.com>
2024-08-20 15:37:40 +0100

    test t0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 /**
  * Displays a message on the STDOUT.
- * @param {String} message The message to display.
+ * @param {string} message The message to display.
  */
 const displayMessage = (message) => {
   console.log(message);

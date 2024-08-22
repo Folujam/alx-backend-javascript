8bd785417c8742068caa14fc845d1204ccb54e37
folujam <olusway@gmail.com>
2024-08-22 22:50:31 +0100

    test 3t

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,6 @@
+const Utils = require('./utils');
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The toatal is: ${total}`);
+};
+module.export = sendPaymentRequestToApi;

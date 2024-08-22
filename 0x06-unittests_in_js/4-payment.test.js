5aab53a4466fae5125f60d34ded9cd094d26e1f1
folujam <olusway@gmail.com>
2024-08-23 00:33:18 +0100

    spy

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,16 +5,16 @@ const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const overhead = sinon.spy(console);
+    const overwatch = sinon.spy(console);
     const stub = sinon.stub(Utils, 'calculateNumber');
 
     stub.returns(10);
     sendPaymentRequestToApi(100, 20);
     expect(stub.calledWith('SUM', 100, 20)).to.be.true;
     expect(stub.callCount).to.be.equal(1);
-    expect(overhead.log.calledWith('The total is: 10')).to.be.true;
-    expect(overhead.log.callCount).to.be.equal(1);
+    expect(overwatch.log.calledWith('The total is: 10')).to.be.true;
+    expect(overwatch.log.callCount).to.be.equal(1);
     stub.restore();
-    overhead.log.restore();
+    overwatch.log.restore();
   });
 });
